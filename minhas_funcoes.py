33457d08f7d4f6e51478ae00fee329e7bcff904a
joelandradesp <joelguarulhos@gmail.com>
2023-01-09 22:05:20 -0300

    Funcao Python minhas_funcoes criado no IDLE para ser usado no import do arquivo teste_import

---
+++ minhas_funcoes.py
@@ -0,0 +1,14 @@
+def soma(n,m):
+    return n + m
+
+def sub(n,m):
+    return n - m
+
+def mult(n,m):
+    return n * m
+
+def nota(n,m):
+    n1 = 0.4 * n
+    n2 = 0.6 * n
+    total = soma(n1, n2)
+    return total
---
+++ teste_import.py
@@ -0,0 +1,5 @@
+import minhas_funcoes as mf
+
+N1 = 10
+N2 = 8
+print(f'Nota Final:  {mf.nota(N1, N2)}')
